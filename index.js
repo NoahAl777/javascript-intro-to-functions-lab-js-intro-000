15e259078b41d9119d580d666fa7cbec57534184
Noah Alston <crashlive112@gmail.com>
2020-07-21 20:44:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-function logUpperCase(string){
-  console.logShout(string)
+function logShout(string){
+  console.log(string)
 }
